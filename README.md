3335e0c9aa214f22e9aef79e39925b7d4e71ef22
Michael Lande Blau <miclande@gmail.com>
2018-04-19 15:31:49 +0200

    solution branch initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practice-express-middlewares
+For educational purposes, an exercise for web dev students
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const catService = require("../cat-service")
 
+const first = (req, res, next) => {
+  res.locals.messages = ["Hello from first!"]
+  next()
+}
+
+const second = (req, res, next) => {
+  res.locals.messages.push("Hello from second")
+  next()
+}
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', first, second, (req, res, next) => {
+  catService.random()
+    .then(fact => {
+      console.log(fact)
+    })
+    .catch(err => next(err))
+
   res.render('index');
 });
 
